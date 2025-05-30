1567fadea8a7becd1ff9408855a9bba7f749fdcd
Ryan Williams <ryan.williams@intersectmbo.org>
2025-05-30 10:34:21 +0100

    vercel deployment

--- cardano-params/next.config.js
+++ cardano-params/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // We'll add the export config back when deploying to GitHub Pages
-  // output: 'export',
-  // images: {
-  //   unoptimized: true,
-  // },
-  // basePath: '/cardano-parameters',
+  // Add any Next.js configuration options here
 }
 
 module.exports = nextConfig 
