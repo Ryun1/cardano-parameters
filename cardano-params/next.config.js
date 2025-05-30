004e8e8bdc96cfdc65614eb74704a35551516c95
Ryan Williams <ryan.williams@intersectmbo.org>
2025-05-30 10:27:50 +0100

    vibe code an app

---
+++ cardano-params/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // We'll add the export config back when deploying to GitHub Pages
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
+  // basePath: '/cardano-parameters',
+}
+
+module.exports = nextConfig 
